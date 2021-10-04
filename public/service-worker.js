ef3f1744e1e7be91c197e56af4bbdac4b822a7ed
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-10-03 22:01:52 -0400

    new update idb

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,9 @@ const VERSION = 'version-01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "./public/index.html",
-    "./public/css/style.css"
+    "./public/css/style.css",
+    "./js/idb.js",
+    "./js/index.js"
 ];
 
 self.addEventListener("install", function (e) {
